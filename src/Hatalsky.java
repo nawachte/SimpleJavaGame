8f4c1c187cad46001e85ceb7a1ff518b69fbeee7
Nicholas Wachter <nawachte@calpoly.edu>
2019-12-02 19:28:57 -0800

    updated hatalsky

--- src/Hatalsky.java
+++ src/Hatalsky.java
@@ -1,6 +1,15 @@
-public class Hatalsky{
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Hatalsky extends MoveEntity{
+    private int charger_part_count = 0;
     private static Hatalsky hatalsky;
-    private Hatalsky(){}
+    private Hatalsky(String id, Point position, List<PImage> images, int imageIndex, int actionPeriod,
+                     int animationPeriod){
+        super(id,position,images,imageIndex, actionPeriod,animationPeriod);
+        this.charger_part_count = charger_part_count;
+    }
     private static Hatalsky getInstance(){
         if (hatalsky == null){
             hatalsky = new Hatalsky();
